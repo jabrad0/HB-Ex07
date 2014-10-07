0651a7c06937d3da3b56aea47057353299f4af2e
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 11:49:53 -0700

    Put code in functions and changed create_word_list function.

--- wordcount.py
+++ wordcount.py
@@ -1,25 +1,44 @@
 from sys import argv
 
-script, filename = argv
+def create_word_list(filename):
+    #word_list = []
 
-word_list = []
+    input_file = open(filename)
 
-input_file = open(filename)
+    # for line in input_file:
+    #     words = line.rstrip().split()
+    #     for word in words:
+    #         word_list.append(word)
 
-for line in input_file:
-    words = line.rstrip().split(" ")
-    for word in words:
-        word_list.append(word)
+    filetext = input_file.read()
+    filetext = filetext.replace('\n', ' ')
+    word_list = filetext.split()
 
-input_file.close()
+    input_file.close()
+    return word_list
 
-word_count = {}
+def create_dictionary(word_list):
+    word_count = {}
 
-for item in word_list:
-    if word_count.get(item):
-        word_count[item] += 1
-    else:
-        word_count[item] = 1
+    for item in word_list:
+        if item in word_count:
+            word_count[item] += 1
+        else:
+            word_count[item] = 1
 
-for k, v in word_count.iteritems():
-    print "%s %d" % (k, v)
+    return word_count
+
+
+def print_dictionary(dictionary):
+    for k, v in dictionary.iteritems():
+        print k, v
+
+def main():
+    filename = argv[1]
+
+    word_list = create_word_list(filename)
+    word_dict = create_dictionary(word_list)
+    print_dictionary(word_dict)
+
+if __name__ == "__main__":
+    main()
