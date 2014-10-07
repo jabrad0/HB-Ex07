a6376847ae3d38737b754b538e8032f3a39ad95e
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 18:09:22 -0700

    Prints words and counts.

--- wordcount.py
+++ wordcount.py
@@ -13,4 +13,13 @@ for line in input_file:
 
 input_file.close()
 
-print word_list
+word_count = {}
+
+for item in word_list:
+    if word_count.get(item):
+        word_count[item] += 1
+    else:
+        word_count[item] = 1
+
+for k, v in word_count.iteritems():
+    print "%s %d" % (k, v)
